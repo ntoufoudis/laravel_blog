e199d456d4523ba31912d50473fcb771fa8e9085
Vasileios Ntoufoudis <info@ntoufoudis.com>
2023-11-14 19:38:31 +0200

    Remove Old Files

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,12 @@
 <?php
 
-use App\Http\Controllers\Auth\AuthenticatedSessionsController;
-use App\Http\Controllers\Auth\ConfirmablePasswordController;
-use App\Http\Controllers\Auth\EmailVerificationNotificationController;
-use App\Http\Controllers\Auth\EmailVerificationPromptController;
-use App\Http\Controllers\Auth\NewPasswordController;
-use App\Http\Controllers\Auth\PasswordController;
-use App\Http\Controllers\Auth\PasswordResetLinkController;
-use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
-use App\Http\Controllers\ProfileController;
 use App\Livewire\Pages\Auth\ConfirmPassword;
 use App\Livewire\Pages\Auth\ForgotPassword;
 use App\Livewire\Pages\Auth\Login;
 use App\Livewire\Pages\Auth\Register;
 use App\Livewire\Pages\Auth\ResetPassword;
+use App\Livewire\Pages\Auth\VerifyEmail;
 use App\Livewire\Pages\Profile;
 use Illuminate\Support\Facades\Route;
 
@@ -29,16 +21,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
+Route::get('/', function () {return view('welcome');});
+Route::get('dashboard', function () {return view('dashboard');})->name('dashboard');
 //    ->middleware(['auth', 'verified'])->name('dashboard');
-
-
 
 // Routes below this point are OK.
 
@@ -47,34 +32,13 @@ Route::middleware('guest')->group(function () {
     Route::get('register', Register::class)->name('register');
     Route::get('forgot-password', ForgotPassword::class)->name('password.request');
     Route::get('reset-password/{token}', ResetPassword::class)->name('password.reset');
-
-
-
-
-
-    Route::post('register', [RegisteredUserController::class, 'store']);
-    Route::post('login', [AuthenticatedSessionsController::class, 'store']);
-    Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])->name('password.email');
-    Route::post('reset-password', [NewPasswordController::class, 'store'])->name('password.store');
 });
 
 Route::middleware('auth')->group(function () {
     Route::get('confirm-password', ConfirmPassword::class)->name('password.confirm');
     Route::get('profile', Profile::class)->name('profile.edit');
-
-
-
-
-   Route::post('logout', [AuthenticatedSessionsController::class, 'destroy'])->name('logout');
-    Route::post('confirm-password', [ConfirmablePasswordController::class, 'store']);
-    Route::post('email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
-        ->middleware('throttle:6,1')
-        ->name('verification.send');
+    Route::get('verify-email', VerifyEmail::class)->name('verification.notice');
     Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)
         ->middleware(['signed', 'throttle:6,1'])
         ->name('verification.verify');
-    Route::get('verify-email', EmailVerificationPromptController::class)->name('verification.notice');
-    Route::put('password', [PasswordController::class, 'update'])->name('password.update');
-    Route::patch('profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
